48df521d618abbc7e55d78ca9301fbd6d055a505
Andrew Hicks <ahicks@frontiersi.com.au>
2021-02-17 09:32:47 +1100

    Inital docs with notebooks

---
+++ conf.py
@@ -0,0 +1,89 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Digital Earth Africa'
+copyright = '2021, DEAfrica Team'
+author = 'DEAfrica Team'
+
+# The full version, including alpha/beta/rc tags
+release = '2021'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "nbsphinx",
+    "sphinx_rtd_theme",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
+                    '**.ipynb_checkpoints',
+                    ]
+
+# Don't execute notebooks
+nbsphinx_execute = 'never'
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+
+try:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_options = {
+        'logo_only': True,
+        'display_version': False,
+        'canonical_url': 'https://docs.digitalearthafrica.org/',
+    }
+except ImportError:
+    html_theme = 'alabaster'
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+html_logo = '_static/logo.png'
+
+html_favicon = '_static/favicon.jpg'
+
+html_use_index = True
+
+# If true, links to the reST sources are added to the pages.
+html_show_sourcelink = False
+
+# Use table-wrapping style
+html_context = {
+    'css_files': [
+        '_static/theme_override.css',  # override wide tables in RTD theme
+        ],
+     }
---
+++ index.rst
@@ -0,0 +1,47 @@
+.. Digital Earth Africa documentation master file, created by
+   sphinx-quickstart on Wed Feb 17 00:08:26 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Digital Earth Africa User Guide
+===============================
+
+.. toctree::
+   :hidden:
+
+   Home <self>
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Beginner's Guide
+   :glob:
+
+   notebooks/Beginners_guide/*
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Datasets
+   :glob:
+
+   notebooks/Datasets/*
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Frequently Used Code
+   :glob:
+
+   notebooks/Frequently_used_code/*
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Real World Examples
+   :glob:
+
+
+   notebooks/Real_world_examples/*
+
+.. toctree::
+  :caption: Index
+
+  genindex
