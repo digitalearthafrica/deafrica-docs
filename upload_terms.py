fc66d99759bcb779005f2989aaa5bae1eecb4fb8
Andrew Hicks <ahicks@frontiersi.com.au>
2021-05-24 17:30:34 +1000

    Update upload_terms.py

--- upload_terms.py
+++ upload_terms.py
@@ -16,7 +16,7 @@ print(f"Before update, {project['name']} (id: {project['id']}) has {project['ter
 
 update_results = client.update_terms(
     project_id=project_id,
-    file_path='./_build/gettext/docs.pot'
+    file_path='./_build/docs.pot'
 )
 
 terms = update_results['terms']
