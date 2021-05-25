315ad8b7c5db729859af1c51052d81aa0b5551fa
Andrew Hicks <ahicks@frontiersi.com.au>
2021-05-25 10:21:28 +1000

    Set baseurl

--- conf.py
+++ conf.py
@@ -118,6 +118,9 @@ try:
 except ImportError:
     html_theme = 'alabaster'
 
+
+html_baseurl = 'https://docs.digitalearthafrica.org/'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
