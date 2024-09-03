537a61c1aa604bf7ee22de7740e63dc69c0a5b0e
nanaboamah89 <nanaboamah89@gmail.com>
2024-09-03 11:59:25 +0000

    update files v2

--- conf.py
+++ conf.py
@@ -91,19 +91,19 @@ autosummary_mock_imports = autodoc_mock_imports
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# # Add any paths that contain templates here, relative to this directory.
+# templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
-                    '**.ipynb_checkpoints',
-                    'sandbox/notebooks/DEAfrica_notebooks_template.ipynb',
-                    ]
+# # List of patterns, relative to source directory, that match files and
+# # directories to ignore when looking for source files.
+# # This pattern also affects html_static_path and html_extra_path.
+# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
+#                     '**.ipynb_checkpoints',
+#                     'sandbox/notebooks/DEAfrica_notebooks_template.ipynb',
+#                     ]
 
-# Don't execute notebooks
-nbsphinx_execute = 'never'
+# # Don't execute notebooks
+# nbsphinx_execute = 'never'
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -111,17 +111,17 @@ nbsphinx_execute = 'never'
 # a list of builtin themes.
 #
 
-# try:
-#     import sphinx_rtd_theme
+try:
+    import sphinx_rtd_theme
 
-#     html_theme = 'sphinx_rtd_theme'
-#     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-#     html_theme_options = {
-#         'logo_only': True,
-#         'display_version': False,
-#     }
-# except ImportError:
-#     html_theme = 'alabaster'
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_options = {
+        'logo_only': True,
+        'display_version': False,
+    }
+except ImportError:
+    html_theme = 'alabaster'
 
 
 # html_baseurl = 'https://docs.digitalearthafrica.org/'
@@ -141,39 +141,39 @@ if os.environ.get("READTHEDOCS", "") == "True":
 # # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 
-# html_logo = '_static/logo.png'
+html_logo = '_static/logo.png'
 
-# html_favicon = '_static/favicon.png'
+html_favicon = '_static/favicon.png'
 
-# html_use_index = True
+html_use_index = True
 
-# # If true, links to the reST sources are added to the pages.
-# html_show_sourcelink = False
+# If true, links to the reST sources are added to the pages.
+html_show_sourcelink = False
 
-# # Use table-wrapping style
-# html_context = {
-#     'css_files': [
-#         '_static/theme_override.css',  # override wide tables in RTD theme
-#     ],
-# }
+# Use table-wrapping style
+html_context = {
+    'css_files': [
+        '_static/theme_override.css',  # override wide tables in RTD theme
+    ],
+}
 
-# # Translation options
-# gettext_compact = "docs"  # makes a single "docs.po" file
-# gettext_location = False  # This causes the build to break?
-# locale_dirs = ['locales/']
+# Translation options
+gettext_compact = "docs"  # makes a single "docs.po" file
+gettext_location = False  # This causes the build to break?
+locale_dirs = ['locales/']
 
-# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-# on_gha = os.environ.get('GITHUB_ACTIONS', None) == 'True'
-# get_translation = os.environ.get('POEDITOR_PROJECT_ID', None) is not None
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_gha = os.environ.get('GITHUB_ACTIONS', None) == 'True'
+get_translation = os.environ.get('POEDITOR_PROJECT_ID', None) is not None
 
-# # If we are on ReadTheDocs, and translation is required, download the translation file from poeditor
-# if on_rtd and get_translation:
-#     import subprocess
-#     subprocess.run(["make", "fetchtranslation"])
+# If we are on ReadTheDocs, and translation is required, download the translation file from poeditor
+if on_rtd and get_translation:
+    import subprocess
+    subprocess.run(["make", "fetchtranslation"])
 
-# # If we are on ReadtheDocs, load the latest version of the notebooks
-# if on_rtd or on_gha:
-#     import subprocess
-#     subprocess.run(["make", "fetchnotebooks"])
-#     subprocess.run(["make", "buildtools"])
+# If we are on ReadtheDocs, load the latest version of the notebooks
+if on_rtd or on_gha:
+    import subprocess
+    subprocess.run(["make", "fetchnotebooks"])
+    subprocess.run(["make", "buildtools"])
 
