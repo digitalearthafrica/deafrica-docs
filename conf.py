72dee1ac112e50645ba5f0fceac3457006e3dc12
nanaboamah89 <nanaboamah89@gmail.com>
2024-09-03 12:46:02 +0000

    update files v9

--- conf.py
+++ conf.py
@@ -112,7 +112,7 @@ nbsphinx_execute = 'never'
 #
 
 try:
-    import sphinx_rtd_theme
+#     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
 
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
