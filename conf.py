ff65d56e5f1588bc3e55d3d3515f7ac14d232308
nanaboamah89 <nanaboamah89@gmail.com>
2024-09-03 13:23:27 +0000

    update files v15

--- conf.py
+++ conf.py
@@ -111,17 +111,17 @@ nbsphinx_execute = 'never'
 # a list of builtin themes.
 #
 
-# try:
-#     import sphinx_rtd_theme
-#     html_theme = 'sphinx_rtd_theme'
+try:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
 
-#     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-#     html_theme_options = {
-#         'logo_only': True,
-#         'display_version': False,
-#     }
-# except ImportError:
-#     html_theme = 'alabaster'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_options = {
+        'logo_only': True,
+        'display_version': False,
+    }
+except ImportError:
+    html_theme = 'alabaster'
 
 
 # Define the canonical URL if you are using a custom domain on Read the Docs
