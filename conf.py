717b6eef165c58010c364e391e3288ebb2f64463
nanaboamah89 <nanaboamah89@gmail.com>
2024-09-03 12:57:12 +0000

    update files v11

--- conf.py
+++ conf.py
@@ -130,23 +130,25 @@ except ImportError:
 # # Add any paths that contain custom static files (such as style sheets) here,
 # # relative to this directory. They are copied after the builtin static files,
 # # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
-# html_logo = '_static/logo.png'
+html_logo = '_static/logo.png'
 
-# html_favicon = '_static/favicon.png'
+html_favicon = '_static/favicon.png'
 
-# html_use_index = True
+html_use_index = True
 
 # # If true, links to the reST sources are added to the pages.
-# html_show_sourcelink = False
+html_show_sourcelink = False
 
 # # Use table-wrapping style
-# html_context = {
-#     'css_files': [
-#         '_static/theme_override.css',  # override wide tables in RTD theme
-#     ],
-# }
+html_context = {
+    'css_files': [
+        '_static/theme_override.css',  # override wide tables in RTD theme
+    ],
+}
+
+
 
 # # Translation options
 # gettext_compact = "docs"  # makes a single "docs.po" file
