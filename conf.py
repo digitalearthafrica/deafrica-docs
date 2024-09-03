ddc72b7b25f61fd09230dd5e5fce57750b654757
nanaboamah89 <nanaboamah89@gmail.com>
2024-09-03 12:38:14 +0000

    update files v7

--- conf.py
+++ conf.py
@@ -114,27 +114,12 @@ nbsphinx_execute = 'never'
 try:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
-    html_theme_options = {
-    'logo_only': False,
-    'display_version': False,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
 
-#     html_theme = 'sphinx_rtd_theme'
 #     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-#     html_theme_options = {
-#         'logo_only': True,
-#         'display_version': False,
-#     }
+    html_theme_options = {
+        'logo_only': True,
+        'display_version': False,
+    }
 except ImportError:
     html_theme = 'alabaster'
 
