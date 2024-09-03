db99cf26cc3e1715d1c2759d6c9aef059a8aa7bd
nanaboamah89 <nanaboamah89@gmail.com>
2024-09-03 11:54:24 +0000

    added files v1

--- conf.py
+++ conf.py
@@ -91,19 +91,19 @@ autosummary_mock_imports = autodoc_mock_imports
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 
-# # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
 
-# # List of patterns, relative to source directory, that match files and
-# # directories to ignore when looking for source files.
-# # This pattern also affects html_static_path and html_extra_path.
-# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
-#                     '**.ipynb_checkpoints',
-#                     'sandbox/notebooks/DEAfrica_notebooks_template.ipynb',
-#                     ]
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
+                    '**.ipynb_checkpoints',
+                    'sandbox/notebooks/DEAfrica_notebooks_template.ipynb',
+                    ]
 
-# # Don't execute notebooks
-# nbsphinx_execute = 'never'
+# Don't execute notebooks
+nbsphinx_execute = 'never'
 
 # -- Options for HTML output -------------------------------------------------
 
