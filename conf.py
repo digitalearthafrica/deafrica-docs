77f043c532b0a6c46209ea35e3411c298687a851
nanaboamah89 <nanaboamah89@gmail.com>
2024-09-04 08:44:40 +0000

    files

--- conf.py
+++ conf.py
@@ -132,6 +132,7 @@ if os.environ.get("READTHEDOCS", "") == "True":
     if "html_context" not in globals():
         html_context = {}
     html_context["READTHEDOCS"] = True
+    
 # # Add any paths that contain custom static files (such as style sheets) here,
 # # relative to this directory. They are copied after the builtin static files,
 # # so a file named "default.css" will overwrite the builtin "default.css".
