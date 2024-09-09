8656a0ba0bc764d81add6ca3d83679f7efc7af8b
nanaboamah89 <nanaboamah89@gmail.com>
2024-09-09 08:29:56 +0000

    added translation

--- conf.py
+++ conf.py
@@ -167,25 +167,25 @@ html_theme_options = {
 
 html_codeblock_linenos_style = 'table'
 
-# # Translation options
-# gettext_compact = "docs"  # makes a single "docs.po" file
-# gettext_location = False  # This causes the build to break?
-# locale_dirs = ['locales/']
+# Translation options
+gettext_compact = "docs"  # makes a single "docs.po" file
+gettext_location = False  # This causes the build to break?
+locale_dirs = ['./locales/']
 
-# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-# on_gha = os.environ.get('GITHUB_ACTIONS', None) == 'True'
-# get_translation = os.environ.get('POEDITOR_PROJECT_ID', None) is not None
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_gha = os.environ.get('GITHUB_ACTIONS', None) == 'True'
+get_translation = os.environ.get('POEDITOR_PROJECT_ID', None) is not None
 
-# # If we are on ReadTheDocs, and translation is required, download the translation file from poeditor
-# if on_rtd and get_translation:
-#     import subprocess
-#     subprocess.run(["make", "fetchtranslation"])
+# If we are on ReadTheDocs, and translation is required, download the translation file from poeditor
+if on_rtd and get_translation:
+    import subprocess
+    subprocess.run(["make", "fetchtranslation"])
 
-# # If we are on ReadtheDocs, load the latest version of the notebooks
-# if on_rtd or on_gha:
-#     import subprocess
-#     subprocess.run(["make", "fetchnotebooks"])
-#     subprocess.run(["make", "buildtools"])
+# If we are on ReadtheDocs, load the latest version of the notebooks
+if on_rtd or on_gha:
+    import subprocess
+    subprocess.run(["make", "fetchnotebooks"])
+    subprocess.run(["make", "buildtools"])
 
 
 
