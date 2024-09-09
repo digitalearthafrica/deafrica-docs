e05de86fe583b4335d583828c2429f91c59d6a2f
nanaboamah89 <nanaboamah89@gmail.com>
2024-09-09 11:42:14 +0000

    update conf

--- conf.py
+++ conf.py
@@ -153,11 +153,11 @@ html_show_sourcelink = False
 # # # Use table-wrapping style
 html_css_files = ['theme_override.css',]  # override wide tables in RTD theme
 
-html_context = {
-    'css_files': [
-        '_static/theme_override.css',  # override wide tables in RTD theme
-    ],
-}
+# html_context = {
+#     'css_files': [
+#         '_static/theme_override.css',  # override wide tables in RTD theme
+#     ],
+# }
 
 ## Use 
 # html_theme_options = {
