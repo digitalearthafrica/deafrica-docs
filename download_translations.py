b6ab82c44239066554532ce410d6574cba9c6b16
Edward Boamah <nanaboamah89@gmail.com>
2024-11-15 10:32:20 +0000

    Update download_translations.py

--- download_translations.py
+++ download_translations.py
@@ -14,12 +14,11 @@ def download_translation(file_path, project_id, api_token):
         file_type='po',
         local_file=file_path,
     )
+if __name__ == '__main__':
+    project_id = os.environ['POEDITOR_PROJECT_ID']
+    api_token = os.environ['POEDITOR_API_TOKEN']
 
+    file_path = 'locales/fr/LC_MESSAGES/docs.po'
+    print(f"Downloading translation to {file_path}")
 
-project_id = os.environ['POEDITOR_PROJECT_ID']
-api_token = os.environ['POEDITOR_API_TOKEN']
-
-file_path = 'locales/fr/LC_MESSAGES/docs.po'
-print(f"Downloading translation to {file_path}")
-
-download_translation(file_path, project_id, api_token)
+    download_translation(file_path, project_id, api_token)
